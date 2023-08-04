2febd14f2125611742d91daacb34970a5f41c4b0
5PK <1trankev@gmail.com>
2023-08-04 11:27:21 -0400

    [ts-u] cleanup

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     preset: "ts-jest",
     testEnvironment: "node",
-    testMatch: ["<rootDir>/test/**/*.tests.js"],
+    testMatch: ["<rootDir>/test/**/*.tests.ts"],
   };
   
